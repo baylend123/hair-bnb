3e2612941c0db3588c799e53e5d4effe5007e074
Nishi <naalnishinelson@gmail.com>
2022-04-04 12:51:12 -0700

    seeds

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,5 @@
 'use strict';
+const bcrypt = require('bcryptjs');
 const {
   Model
 } = require('sequelize');
@@ -13,6 +14,54 @@ module.exports = (sequelize, DataTypes) => {
       User.hasMany(models.Booking, {foreignKey : 'userId'})
       User.hasMany(models.Message, {foreignKey : 'userId'})
     }
+
+    toSafeObject(){
+      const {
+        id, firstName, lastName, email, bio, currentHairStyle, profilePhoto, city, state
+      } = this;
+      return {
+        id, firstName, lastName, email, bio, currentHairStyle, profilePhoto, city, state
+      };
+    }
+
+    validatePassword(password) {
+      bcrypt.compareSync(password, this.hashedPassword.toString())
+    }
+
+    async getCurrentUserById(id) {
+      return await User.scope('currentUser').findByPk(id);
+     };
+
+    async login({ credential, password }) {
+      const { Op } = require('sequelize');
+      const user = await User.scope('loginUser').findOne({
+        where: {
+          [Op.or]: {
+            email: credential,
+          },
+        },
+      });
+      if (user && user.validatePassword(password)) {
+        return await User.scope('currentUser').findByPk(user.id);
+      }
+    };
+
+    async signup({ firstName, lastName, email, bio, currentHairStyle, profilePhoto, hashedPassword, city, state }) {
+      const hashedPassword = bcrypt.hashSync(password);
+      const user = await User.create({
+        firstName,
+        lastName,
+        email,
+        bio,
+        currentHairStyle,
+        profilePhoto,
+        hashedPassword,
+        city,
+        state
+      });
+      return await User.scope('currentUser').findByPk(user.id);
+    };
+
   }
   User.init({
     firstName: DataTypes.STRING,
@@ -21,7 +70,9 @@ module.exports = (sequelize, DataTypes) => {
     bio: DataTypes.TEXT,
     currentHairStyle: DataTypes.STRING,
     profilePhoto: DataTypes.STRING,
-    hashedPassword: DataTypes.STRING
+    hashedPassword: DataTypes.STRING,
+    city: DataTypes.STRING,
+    state: DataTypes.STRING
   }, {
     sequelize,
     modelName: 'User',
---
+++ backend/seeders/20220404192243-tagSeed.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+     await queryInterface.bulkInsert('Tags', [{
+      name: 'long hair',
+      stylistId: 1
+    }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+
+    const Op = Sequelize.Op;
+     return queryInterface.bulkDelete('Tags', {
+       name: { [Op.in]: ['long hair'] }
+    }, {});
+  }
+};

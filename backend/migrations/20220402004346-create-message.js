4eb99d08fe8a1ae8d3909d4049e47de5d213b54a
Nishi <naalnishinelson@gmail.com>
2022-04-05 11:03:01 -0700

    finishedModels

--- backend/migrations/20220402004346-create-message.js
+++ backend/migrations/20220402004346-create-message.js
@@ -12,7 +12,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.INTEGER
       },
-      stylisId: {
+      stylistId: {
         allowNull: false,
         type: Sequelize.INTEGER
       },
---
+++ backend/seeders/20220405172051-bookingSeeders.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+     await queryInterface.bulkInsert('Bookings', [{
+        date: new Date(),
+        stylistId: 1,
+        userId: 2
+     }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+
+     const Op = Sequelize.Op;
+     return queryInterface.bulkDelete('Bookings', {
+       id: { [Op.in]: [1] }
+     }, {});
+  }
+};

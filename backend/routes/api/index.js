e746d209b18186c640cb53d869eaa42a3dd87c7f
baylen doss <baylendoss@baylens-Air.lan>
2022-04-06 12:26:46 -0500

    App Spun Up

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,14 +1,12 @@
 const router = require('express').Router();
 const asyncHandler = require('express-async-handler');
 const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../models');
+const { User, Tag } = require('../../models');
 const { requireAuth } = require('../../utils/auth.js');
 const sessionRouter = require('./session.js');
 
 
 router.use('/session', sessionRouter);
-
-
 router.post('/test', function (req, res) {
   res.json({ requestBody: req.body });
 });
@@ -16,11 +14,20 @@ router.post('/test', function (req, res) {
 router.get('/set-token-cookie', asyncHandler(async (req, res) => {
   const user = await User.findOne({
     where: {
-      username: 'Demo-lition'
+      email: 'bm@email.com'
     },
   })
   setTokenCookie(res, user);
   return res.json({ user });
 }));
+
+router.get('/query-sandbox', asyncHandler(async (req, res) => {
+  const tag = await Tag.findByPk(1)
+  console.log(tag)
+  res.json({ tag })
+}))
+
+
+
 
 module.exports = router;

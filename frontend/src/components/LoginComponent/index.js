d3472aa7259c66e9eb46974c8642fdb72cd5b40a
Nishi <naalnishinelson@gmail.com>
2022-04-13 16:05:28 -0700

    login-modal-works

--- frontend/src/components/LoginComponent/index.js
+++ frontend/src/components/LoginComponent/index.js
@@ -1,16 +1,37 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import * as sessionActions from '../../store/session'
-import {useDispatch, useSelector} from 'react-redux'
-import {Redirect} from 'react-router-dom'
+import {useDispatch, useSelector} from 'react-redux';
+import {Redirect} from 'react-router-dom';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import TextField from '@mui/material/TextField';
 
 
-function LoginComponent (){
 
-    const dispatch = useDispatch()
-    const sessionUser = useSelector(state => state.session.user)
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [errors, setErrors] = useState([])
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+
+function LoginComponent() {
+    const dispatch = useDispatch();
+    const sessionUser = useSelector(state => state.session.user);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState([]);
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
     if(sessionUser) return (
         console.log('It works')
@@ -26,24 +47,65 @@ function LoginComponent (){
     }
 
     return (
-        <form onSubmit={login}>
-            <ul>
-                {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-            </ul>
-            <label>
-                Email
-                <input type='text' value={email} onChange={(e) => setEmail(e.target.value)} required />
-            </label>
-            <label>
-                Password
-                <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} required />
-            </label>
-            <button type='submit'>
-                Log In
-            </button>
-        </form>
+        <div>
+            <Button onClick={handleOpen}>Log In</Button>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box
+                    component="form"
+                    sx={style}
+                    onSubmit={login}
+                    autoComplete="off"
+                >
+                    <ul>
+                        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+                    </ul>
+                    <div>
+                        <TextField
+                            id="outlined-basic"
+                            label="Email"
+                            type="text"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                        <TextField
+                            id="outlined-password-input"
+                            label="Password"
+                            type="password"
+                            autoComplete="current-password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                        <button type='submit'>
+                            Log In
+                        </button>
+                    </div>
+                </Box>
+            </Modal>
+        </div>
     )
 
 }
 
 export default LoginComponent
+
+
+
+
+    // <label>
+    // Email
+    // <input type='text' value={email} onChange={(e) => setEmail(e.target.value)} required />
+    // </label>
+    // <label>
+    // Password
+    // <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} required />
+    // </label>
+    // <button type='submit'>
+    // Log In
+    // </button>

a4196e58d1f666a8e154af9b5bd8633cb69533c6
baylen doss <baylendoss@baylens-Air.lan>
2022-04-29 13:56:11 -0500

    stylist signup page 3

--- frontend/src/components/StylistSignUp/Page-2/index.js
+++ frontend/src/components/StylistSignUp/Page-2/index.js
@@ -1,46 +1,55 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom'
-import {useState}  from 'react'
+import { useState, useEffect } from 'react'
 import { csrfFetch } from "../../../store/csrf.js";
 import homeicon from '../../../images/home-icon.png'
 import './PageTwo.css'
 
-export default function PageTwo({ changePage }) {
+export default function PageTwo({ changePage, pageTwoState, setPageTwoState }) {
     const navigate = useNavigate()
-    const [profilePhoto, setProfilePhoto] = useState()
+    const [upload, setUpload] = useState(false)
     const handleProfilePhoto = async (e) => {
         const formData = new FormData();
         formData.append('profilePhoto', e.target.files[0])
         let response = await csrfFetch('/api/image-upload', {
             method: 'POST',
-            headers : {'Content-Type' : 'multipart/form-data'},
+            headers: { 'Content-Type': 'multipart/form-data' },
             body: formData,
-          });
+        });
         response = await response.json()
-        console.log(response)
-        await setProfilePhoto(response.profilePhoto)
+        pageTwoState.profilePhoto = response.profilePhoto
+        setUpload(true)
+        await setPageTwoState(pageTwoState)
     }
     return (
         <div className='stylist-sign-up-container'>
             <div className="ssup-image">
                 <img onClick={() => navigate('/')} src={homeicon} alt='home icon' />
             </div>
-
-            <h1 className="ssup-h1">Now lets create your profile</h1>
-            <div>
+            <div className="ssup-page-2-container">
+                <h1 className="ssup-h1">Now lets create your profile</h1>
                 <label className="ssup-label">First choose a profile photo</label>
-                <img src={profilePhoto} alt="profile"/>
+                {upload &&<img className="ssup-img" src={pageTwoState.profilePhoto} alt="profile" />}
                 <input
-                onChange={handleProfilePhoto}
-                type="file" />
+                    onChange={handleProfilePhoto}
+                    type="file" />
+                <label className="ssup-label">Tell us a little bit about yourself, and you styling skills</label>
+                <textarea 
+                onChange={(e) => {
+                    pageTwoState.bio = e.target.value
+                    setPageTwoState(pageTwoState)
+                }}
+                className="ssup-bio"
+                />
             </div>
-            
-            <div>
+
+
+            <div className="ssup-p2-button-container">
                 <button className="ssup-next"
-                onClick={() => changePage((prev) => prev -1)}
+                    onClick={() => changePage((prev) => prev - 1)}
                 >Go Back</button>
                 <button className="ssup-next"
-                onClick={() => changePage((prev) => prev + 1)}
+                    onClick={() => changePage((prev) => prev + 1)}
                 >next</button>
             </div>
         </div>
--- frontend/src/components/StylistSignUp/index.js
+++ frontend/src/components/StylistSignUp/index.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import './StylistSignup.css'
 import PageOne from "./Page-1";
 import PageTwo from "./Page-2"
+import PageThree from "./Page-3";
 export default function StylistSignUp() {
     const [page, changePage] = useState(1)
     const [pageOneState, setPageOneState] = useState({
@@ -12,7 +13,11 @@ export default function StylistSignUp() {
         password : '',
         confirmPassword : ''
     })
-    console.log(pageOneState)
+    const [pageTwoState, setPageTwoState] = useState({
+        profilePhoto : '', 
+        bio : ''
+    })
+    const [pageThreeState, setPageThreeState] = useState([])
     return (
         <>
         {page === 1 && 
@@ -22,7 +27,17 @@ export default function StylistSignUp() {
         changePage={changePage} />
         }
         {page === 2 &&
-        <PageTwo changePage={changePage}  />
+        <PageTwo changePage={changePage} 
+        pageTwoState={pageTwoState}
+        setPageTwoState={setPageTwoState}
+        />
+        }
+        {page === 3 &&
+        <PageThree
+        changePage={changePage}
+        pageThreeState={pageThreeState}
+        setPageThreeState={setPageThreeState}
+        />
         }
         </>
     )
